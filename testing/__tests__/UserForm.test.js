74c690aaa62983aa580d539ea149a9ba6c613479
Joel Kong <joelkong2001@gmail.com>
2023-06-28 20:52:00 +0800

    chore: added test to check if submit is working

--- testing/__tests__/UserForm.test.js
+++ testing/__tests__/UserForm.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import user from "@testing-library/user-event";
 import UserForm from "../components/UserForm";
 
-test("It shows two inputs and a button", () => {
+test("It shows two inputs and a button", async () => {
   // render the component
   render(<UserForm />);
 
@@ -13,4 +14,27 @@ test("It shows two inputs and a button", () => {
   // Assertion - make sure component is doing what it is expected to do
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+test("It calls onUserAdd function when the form is submitted", async () => {
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+  render(<UserForm onUserAdd={callback} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  await user.click(nameInput);
+  await user.keyboard("Joel");
+  await user.click(emailInput);
+  await user.keyboard("joelkong2001@gmail.com");
+  await user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({
+    name: "Joel",
+    email: "joelkong2001@gmail.com",
+  });
 });
